880a20c1700765e2cdda52fa782062dfb55b05b8
junetype <40350002+junetype@users.noreply.github.com>
2021-06-21 19:27:06 +0530

    Update url_send.sh

--- url_send.sh
+++ url_send.sh
@@ -4,21 +4,16 @@ printf "Installing RDP Be Patience... " >&2
 sudo useradd -m URLSEND
 sudo adduser URLSEND sudo
 echo 'URLSEND:passwd1!' | sudo chpasswd
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt update
 sudo apt-get update
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg --install google-chrome-stable_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
-sudo adduser URLSEND chrome-remote-desktop
+sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb 
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb \n
+ sudo DEBIAN_FRONTEND=noninteractive sudo apt install -y --fix-broken 
+sudo DEBIAN_FRONTEND=noninteractive apt install -y xfce4 desktop-base 
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session' 
+sudo apt install -y xscreensaver 
+sudo groupadd chrome-remote-desktop 
+sudo usermod -a -G chrome-remote-desktop $USER 
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
@@ -32,3 +27,4 @@ then
 else
     printf "\n\nError Occured " >&2
 fi
+
